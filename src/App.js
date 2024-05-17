ea14d8089d96eadd5f161b5741be05c84f597147
VAMSI <20551a4251.vamsi@gmail.com>
2024-05-17 23:07:15 +0530

    uploaded

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [task,setTask]=useState('');
+  const [todos,setTodos]=useState([])
+  const changeHandler=e=>{
+    setTask(e.target.value)
+  }
+  const submitHandler=e=>{
+    e.preventDefault();
+    const newTodos = [...todos,task]
+    setTodos(newTodos);
+    setTask("")
+  }
+  const deleteHandler=(indexValue)=>{
+    const newTodos=todos.filter((todo,index)=>index !== indexValue);
+    setTodos(newTodos)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <center>
+      <div className="card">
+        <div className="card-body">
+          <h3 className="card-title">TODO LIST APPLICATION</h3>
+          <form onSubmit={submitHandler}>
+            <input type="text" value={task} onChange={changeHandler}  />
+            <input type="submit" name='Add' value="Add" />
+          </form>
+          <div>
+            {
+              todos.map((todo,index)=>
+              <div>
+                <h5 key={index}>{todo} &nbsp; <button onClick={()=>deleteHandler(index)}>❌</button></h5>
+              </div>
+              )
+            }
+          </div>
+        </div>
+      </div>
+    </center>
+    </>
   );
 }
 
